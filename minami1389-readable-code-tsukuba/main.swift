5afddd796d0af298fdc21ff1a6e237a08be5f34f
minami <s1311389@coins.tsukuba.ac.jp>
2015-07-04 10:26:34 +0900

    add new file

---
+++ minami1389-readable-code-tsukuba/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  minami1389-readable-code-tsukuba
+//
+//  Created by minami on 7/4/15.
+//  Copyright (c) 2015 minami. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
